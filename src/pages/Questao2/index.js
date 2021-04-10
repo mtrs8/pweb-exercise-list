08c34aae95dce9a32d9d0090622e1b5aa8c0e6eb
Matheus A. Rodrigues <mrodrigues@fapex.org.br>
2021-04-10 19:56:00 -0300

    Questão 2

--- src/pages/Questao2/index.js
+++ src/pages/Questao2/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 function Questao2() {
@@ -7,15 +8,25 @@ function Questao2() {
     const [gorjeta, setGorjeta] = useState();
 
     function calcularNF(event) {
-
+        let valorTotal = parseFloat(total.replace(',', '.'));
+        let numeroDePessoas = parseInt(numPessoas.replace(',', '.'));
+        let percentGorjeta = parseFloat(gorjeta.replace(',', '.'));
+        const totalPorPessoa = (valorTotal / numeroDePessoas).toFixed(2);
+        const gorjeta = (valorTotal*percentGorjeta)/100;
+        event.preventDefault();
+        alert(`Valor total da Mesa: ${valorTotal}
+        \nValor por Pessoa: ${totalPorPessoa}
+        \nValor da Gorjeta: ${gorjeta}`);
     }
-
+    
     return (
         <div className="page-questao2">
+             <Link to="/">Voltar para Home Page</Link>
+            <h1>Questão 02 - Calcular Nota Fiscal</h1>
             <form onSubmit={calcularNF}>
-                <input type="text" value={total} onChange={e => setTotal(e.target.value)} placeholder="Total Gasto"/>
-                <input type="text" value={numPessoas} onChange={e => setNumPessoas(e.target.value)} placeholder="Total Pessoas"/>
-                <input type="text" value={gorjeta} onChange={e => setGorjeta(e.target.value)} placeholder="Gorjeta"/>
+                <input type="text" value={total} onChange={e => setTotal(e.target.value)} placeholder="Total Gasto" required/>
+                <input type="text" value={numPessoas} onChange={e => setNumPessoas(e.target.value)} placeholder="Total Pessoas" required/>
+                <input type="text" value={gorjeta} onChange={e => setGorjeta(e.target.value)} placeholder="Gorjeta em %" required/>
                 <button type="submit"> Calcular Nota Fiscal</button>
             </form>
         </div>
